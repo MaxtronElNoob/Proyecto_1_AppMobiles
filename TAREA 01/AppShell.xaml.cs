8a6b5e9672575f7bfcab91caa66966dc756a8b6a
ClaudiaGM <claudia.guzmanm@usm.cl>
2025-04-16 17:46:42 -0400

    Primera version

---
+++ TAREA 01/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TAREA_01;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TAREA 01/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace TAREA_01;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		UpdateColor(); // inicializar al cargar
+	}
+
+	private void OnSliderChanged(object sender, ValueChangedEventArgs e)
+    {
+        UpdateColor();
+    }
+	private void OnRandomColorClicked(object sender, EventArgs e)
+    {
+        var rand = new Random();
+        RedSlider.Value = rand.Next(0, 256);
+        GreenSlider.Value = rand.Next(0, 256);
+        BlueSlider.Value = rand.Next(0, 256);
+    }
+
+	private void UpdateColor()
+    {
+        int r = (int)RedSlider.Value;
+        int g = (int)GreenSlider.Value;
+        int b = (int)BlueSlider.Value;
+
+        Color newColor = Color.FromRgb(r, g, b);
+        ColorPreview.Color = newColor;
+        HexLabel.Text = $"#{r:X2}{g:X2}{b:X2}";
+		this.BackgroundColor = newColor;
+    }
+}
+
